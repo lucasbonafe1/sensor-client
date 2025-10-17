a024332836192cd41c3fba66a15b38f7fac86507
lucasbonafe1 <l2005bonafe@gmail.com>
2025-10-17 15:50:07 -0300

    feat: mudanças na estilização e estrutura

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
-import { SensorDataService } from '../../services/sensor-data.service';
 import { FormsModule } from '@angular/forms';
+import { SearchBarComponent } from "../../components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [FormsModule, SearchBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  sensorState: string = '';
-
-  constructor(private sensorService: SensorDataService) {}
-
-  onSearch(state: string) {
-    this.sensorState = state;
-
-    this.sensorService.getSensorByState(state).subscribe(data => {
-      console.log('Sensor data for state:', state, JSON.stringify(data, null, 2));
-    });
-  } 
 }
